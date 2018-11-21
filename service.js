186d2b77922933a7098c80c4f53461ce97562c38
Matt Larson <matt.a.larson@gmail.com>
2018-11-21 07:29:29 -0600

    init commit

---
+++ service.js
@@ -0,0 +1,44 @@
+const url = require('url')
+
+exports.sampleRequest = function (req, res) {
+    const reqUrl = url.parse(req.url, true)
+    let name = 'World'
+    if (reqUrl.query.name) {
+        name = reqUrl.query.name
+    }
+
+    let response = {
+        'text': `Hello ${name}`
+    }
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(response))
+}
+
+exports.testRequest = function (req, res) {
+  let body = ''
+
+  req.on('data', function (chunk) {
+      body += chunk
+  })
+
+  req.on('end', function () {
+
+      let postBody = JSON.parse(body)
+
+      let response = {
+          'text': `Post Request Value is ${postBody.value}`
+      }
+
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify(response))
+  })
+
+  exports.invalidRequest = function (req, res) {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Invalid Request')
+  }
+}
